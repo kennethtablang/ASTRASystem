3882026f89b1be7b68f127c1c201c594a802def4
kennethtablang <kennethreytablang@gmail.com>
2025-12-06 11:05:28 +0800

    Confirm Order Simple Dto update

---
+++ ASTRASystem/DTO/Order/ConfirmOrderSimpleDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASTRASystem.DTO.Order
+{
+    public class ConfirmOrderSimpleDto
+    {
+        public string? Notes { get; set; }
+    }
+
+    public class SimpleNotesDto
+    {
+        public string? Notes { get; set; }
+    }
+
+    public class SimpleReasonDto
+    {
+        [Required]
+        public string Reason { get; set; }
+    }
+}
