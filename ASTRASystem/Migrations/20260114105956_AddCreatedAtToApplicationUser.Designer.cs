b3dead6360624a4c21887462038f5cb05bbd4431
Kenneth Tablang <kennethreytablang@gmail.com>
2026-01-15 18:56:58 +0800

    Adding the CreatedAt to the ApplicationUsers

--- ASTRASystem/Migrations/20260114105956_AddCreatedAtToApplicationUser.Designer.cs
+++ ASTRASystem/Migrations/20260114105956_AddCreatedAtToApplicationUser.Designer.cs
@@ -4,6 +4,7 @@ using ASTRASystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ASTRASystem.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260114105956_AddCreatedAtToApplicationUser")]
+    partial class AddCreatedAtToApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
